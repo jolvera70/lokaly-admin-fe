cee2ae5e16f70f6ff987830f7e9838b821bd8266
Jorge Olvera <jolvera70@gmail.com>
2026-01-16 17:44:38 -0600

    integrando inicio panel de administracion vendedor

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
 server: {
   proxy: {
     "/api/public": {
